b0d8815305154748b58b1975476d963a927655ff
SzymonSt <szymonst2808@gmail.com>
2024-09-18 23:24:36 +0200

    alert schema testing validation

--- src/alertwebhook/main.go
+++ src/alertwebhook/main.go
@@ -21,17 +21,20 @@ import (
 	"net/http"
 )
 
+func webhookHandler(w http.ResponseWriter, r *http.Request) {
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+	var buf bytes.Buffer
+	if err := json.Indent(&buf, b, " >", "  "); err != nil {
+		panic(err)
+	}
+	log.Println(buf.String())
+}
+
 func main() {
-	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		b, err := io.ReadAll(r.Body)
-		if err != nil {
-			panic(err)
-		}
-		defer r.Body.Close()
-		var buf bytes.Buffer
-		if err := json.Indent(&buf, b, " >", "  "); err != nil {
-			panic(err)
-		}
-		log.Println(buf.String())
-	})))
+	http.HandleFunc("/api/webhook", webhookHandler)
+	log.Fatal(http.ListenAndServe(":5002", nil))
 }
